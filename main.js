c62bb2c8f163672250cee4db85b1cb98e113240d
tacyeet <tacyeet@gmail.com>
2026-03-05 10:36:16 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-import {engine} from "./music/engine.js"
-
-await engine.init()
-
-document.getElementById("play").onclick=()=>engine.start()
-document.getElementById("pause").onclick=()=>engine.stop()
-
-document.getElementById("tempo").oninput=e=>engine.setTempo(e.target.value)
